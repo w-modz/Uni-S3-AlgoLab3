40b5ff465b420e7f225258b97f110f90a73e2998
w-modz <46196436+w-modz@users.noreply.github.com>
2023-11-30 15:40:44 +0100

    Add file header, implement actual_size, implement Clear()

--- DynamicArray/DynamicArray/DynamicArray.cpp
+++ DynamicArray/DynamicArray/DynamicArray.cpp
@@ -1,3 +1,6 @@
+// ALGO2 IS1 221A LAB02
+// Wiktor Modzelewski
+// mw53766@zut.edu.pl
 #include <iostream>
 #include <string>
 #include <array>
@@ -13,10 +16,11 @@ class DynamicArray
 private:
 	T* values;
 	uint32_t size = DEFAULT_SIZE;
+	uint32_t actual_size = 0;
 public:
 	DynamicArray(void)
 	{
-		values = new T[DEFAULT_SIZE](NULL);
+		values = new T[DEFAULT_SIZE];
 	}
 
 	~DynamicArray()
@@ -29,9 +33,14 @@ public:
 	{
 		return size;
 	}
+
+	uint32_t GetActualSize()
+	{
+		return actual_size;
+	}
 	void Set(uint32_t index, T value)
 	{
-		if (index >= size)
+		if (index > actual_size)
 		{
 			throw std::out_of_range("Index out of bounds");
 		}
@@ -39,7 +48,7 @@ public:
 	}
 	T Get(uint32_t index)
 	{
-		if (index >= size)
+		if (index >= actual_size)
 		{
 			throw std::out_of_range("Index out of bounds");
 		}
@@ -48,42 +57,30 @@ public:
 
 	void Append(T value)
 	{
-		int last_non_null_index = 0;
-		for (int i = 0; i < size; i++)
-		{
-			if (Get(i) != NULL)
-			{
-				last_non_null_index = i;
-			}
-		}
-		if (last_non_null_index + 1 >= size)
+		if (actual_size + 1 > size)
 		{
 			Expand();
 		}
-		Set(last_non_null_index + 1, value);
+		Set(actual_size, value);
+		actual_size++;
 	}
 
 	void Clear()
 	{
-		for (int i = 0; i < size; i++)
-		{
-			Set(i, NULL);
-		}
+		delete[] values;
+		actual_size = 0;
+		T* new_values = new T[size];
+		values = new_values;
 	}
 
 	// The sorting method assumes existance of valid ">" operator overload for given datatype
 	void Sort()
 	{
-		for (int i = 0; i < size - 1; i++)
+		for (int i = 0; i < actual_size - 1; i++)
 		{
-			for (int j = 0; j < size - i - 1; j++)
+			for (int j = 0; j < actual_size - i - 1; j++)
 			{
-				if (values[j] == NULL && values[j + 1] != NULL)
-				{
-					values[j] = values[j + 1];
-					values[j + 1] = NULL;
-				}
-				else if (values[j] > values[j + 1] && values[j + 1] != NULL)
+				if (values[j] > values[j + 1])
 				{
 					int temp = values[j + 1];
 					values[j + 1] = values[j];
@@ -99,33 +96,19 @@ public:
 		{
 			return "[]";
 		}
-		else if (size == 0)
+		else if (actual_size == 0)
 		{
 			return "[]";
 		}
-		else if (size < number_to_print)
+		else if (actual_size < number_to_print)
 		{
 			throw std::out_of_range("Index out of bounds");
 		}
 		std::ostringstream output_stream;
-		if (values[0] == NULL)
-		{
-			output_stream << "[NULL";
-		}
-		else
-		{
-			output_stream << "[" << values[0];
-		}
+		output_stream << "[" << values[0];
 		for (int i = 1; i < number_to_print; i++)
 		{
-			if (values[i] == NULL)
-			{
-				output_stream << ", NULL";
-			}
-			else
-			{
-				output_stream << ", " << values[i];
-			}
+			output_stream << ", " << values[i];
 		}
 		output_stream << "]\n";
 		return output_stream.str();
@@ -135,8 +118,8 @@ private:
 	void Expand()
 	{
 		const uint32_t new_size = size * EXPANSION_MULTIPLIER;
-		T* new_values = new T[new_size](NULL);
-		for (int i = 0; i < size; i++)
+		T* new_values = new T[new_size];
+		for (int i = 0; i < actual_size; i++)
 		{
 			new_values[i] = values[i];
 		}
@@ -151,8 +134,8 @@ int main()
 	std::default_random_engine gen(time(nullptr));
 	std::uniform_int_distribution<int> dist(-5, 5);
 
-	DynamicArray<double>* da = new(DynamicArray<double>);
-	const int order = 5; // maximum order of data input (in powers of 10)
+	DynamicArray<std::pair<int,char>>* da = new DynamicArray<std::pair<int,char>>;
+	const int order = 7; // maximum order of data input (in powers of 10)
 	const int n = pow(10, order); // data sample size
 	// appending to table
 	clock_t t1 = clock();
@@ -160,22 +143,21 @@ int main()
 
 	for (int i = 0; i < n; i++) 
 	{
-		std::cout << "Appending " << i << " out of " << n << "\n";
-
-		double so = rand() % 100 + 1;
 		clock_t t1_element = clock();
-		da->Append(so);
+		da->Append(std::make_pair(rand(), (char) rand() ));
 		clock_t t2_element = clock();
 		double time_per_element = t2_element - t1_element;
 		if (time_per_element > max_time_per_element)
 		{
 			max_time_per_element = time_per_element;
+			std::cout << max_time_per_element << " ms \n";
 		}
 	}
 	clock_t t2 = clock();
 
 	// display of number of elements added, calculation of total time taken and average time per element
 	std::cout << "Array lenght: " << da->GetSize() << "\n";
+	std::cout << "Array actual lenght: " << da->GetActualSize() << "\n";
 	std::cout << "Total time of appending = " << ((t2 - t1) / (double)CLOCKS_PER_SEC) * 1000 << "ms\n";
 	std::cout << "Average time of appending = " << ((t2 - t1) / (double)CLOCKS_PER_SEC) * 1000 / n << "ms\n";
 	std::cout << "Worst time of appending = " << max_time_per_element << "ms\n";
